1516cf87d8604c8c540d6f9f23db0b362d72c410
Robu Corina <corinaflorentina28@gmail.com>
2024-03-28 17:20:28 +0200

    lab5

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        String s1 = new String("This is a string");
+        String s2 = new String("This is a string");
+        if (s1.equals(s2)) {  // cand e cu new doar la equals e true
+            System.out.println("1) => Strings are equal!");
+        }
+        if (s1 == s2) {
+            System.out.println("1) => Strings are the same!");
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        String s3 = "This is a string";
+        String s4 = "This is a string";
+        if (s3.equals(s4)) {
+            System.out.println("2) => Strings are equal!");
+        }
+        if (s3 == s4) {
+            System.out.println("2) => Strings are the same!");
+        }
+
+        if (s1.equals(s4)) {
+            System.out.println("3) => Strings are equal!");
         }
     }
 }
